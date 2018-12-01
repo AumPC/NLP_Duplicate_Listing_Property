4ea479d32b5f936b8a7f739a89a7b8ea54610c04
JewJew Thanawat <g-o.v-nus@hotmail.com>
2018-12-02 05:48:04 +0700

    trim bag of word
    - optimize detail_similarity
    - refactor some code

--- Similarity.py
+++ Similarity.py
@@ -8,34 +8,32 @@ from time import time
 
 
 def different_numerical(a, b):
-    if a is None and b is None:
+    if a is b:
         return 1
     if a is None or b is None:
         return 0
-    if a == 0 and b == 0:
-        return 1
     return 1 - (abs(a - b) * 2 / (a + b))
 
 
 def different_character(a, b):
     # Levenshtein distance
-    metrix = []
+    matrix = []
     for i in range(len(a) + 1):
         m = []
         for j in range(len(b) + 1):
             m.append(0)
-        metrix.append(m)
+        matrix.append(m)
     for i in range(len(a) + 1):
-        metrix[i][0] = i
+        matrix[i][0] = i
     for i in range(len(b) + 1):
-        metrix[0][i] = i
+        matrix[0][i] = i
     for i in range(len(a)):
         for j in range(len(b)):
             if a[i] == b[j]:
-                metrix[i+1][j+1] = metrix[i][j]
+                matrix[i+1][j+1] = matrix[i][j]
             else:
-                metrix[i+1][j+1] = min(metrix[i][j], metrix[i+1][j], metrix[i][j+1]) + 1
-    return 1 - (metrix[len(a)][len(b)] / max(len(a), len(b)))
+                matrix[i+1][j+1] = min(matrix[i][j], matrix[i+1][j], matrix[i][j+1]) + 1
+    return 1 - (matrix[len(a)][len(b)] / max(len(a), len(b)))
 
 
 def field_similarity(a, b, weight):
@@ -47,24 +45,14 @@ def field_similarity(a, b, weight):
     return price_score + size_score + tower_score + floor_score + type_score
 
 
-def detail_similarity(bag_A, bag_B):
-    size_A = 0
-    size_B = 0
-    intersect = 0
-    for word in bag_A:
-        size_A += bag_A[word]
-    for word in bag_B:
-        size_B += bag_B[word]
-        if word in bag_A:
-            intersect += min(bag_A[word], bag_B[word])
-    return (1+intersect)/(1+min(size_A, size_B))
+def detail_similarity(a, b):
+    size_a = sum(a.values())
+    size_b = sum(b.values())
+    intersect = sum([min(a[word], b[word]) for word in set(a.keys()).intersection(set(b.keys()))])
+    return (1+intersect)/(1+min(size_a, size_b))
 
 
 def score_calculate(docs, parameter):
-    weight = parameter['weight']
-    min_confidence = parameter['min_confidence']
-    hard_threshold = parameter['hard_threshold']
-    soft_threshold = parameter['soft_threshold']
     tokenize_time = 0
     calculate_time = 0
     duplicate = {}
@@ -76,26 +64,27 @@ def score_calculate(docs, parameter):
         a = time()
         word_list = word_tokenize(doc['detail'], engine='newmm')
         b = time()
-        doc['detail'] = {k: v for k, v in Counter(word_list).items() if not (k.isspace() or k.replace('.','',1).isdecimal())}
+        word_list = {k: v for k, v in Counter(word_list).items() if not (k.isspace() or k.replace('.','',1).isdecimal())}
+        doc['detail'] = dict(Counter(word_list).most_common(parameter['most_frequency_word']))
         tokenize_time += b - a
         for calculated_doc in calculated_docs:
-            field_score = field_similarity(doc, calculated_doc, weight)
+            field_score = field_similarity(doc, calculated_doc, parameter['weight'])
             detail_score = detail_similarity(doc['detail'], calculated_doc['detail'])
             length_weight = 1 / (1 + log(1 + (doc['detail_length'] + calculated_doc['detail_length']) / 2, 10))
             confidence = (length_weight * field_score) + ((1 - length_weight) * detail_score)
-            if confidence >= hard_threshold:
+            if confidence >= parameter['hard_threshold']:
                 if calculated_doc['id'] in duplicate:
                     duplicate[calculated_doc['id']].append(doc['id'])
                 else:
                     duplicate[calculated_doc['id']] = [calculated_doc['id'], doc['id']]
                 is_duplicate = True
                 break
-            if confidence >= min_confidence:
+            if confidence >= parameter['min_confidence']:
                 score.append([doc['id'], calculated_doc['id'], confidence])
         if not is_duplicate:
             calculated_docs.append(doc)
         calculate_time += time() - b
     score = sorted(score, key=itemgetter(2), reverse=True)
-    medium_score = [s for s in score if s[2] >= soft_threshold]
-    weak_score = [s for s in score if s[2] < soft_threshold]
+    medium_score = [s for s in score if s[2] >= parameter['soft_threshold']]
+    weak_score = [s for s in score if s[2] < parameter['soft_threshold']]
     return duplicate, medium_score, weak_score, tokenize_time, calculate_time
